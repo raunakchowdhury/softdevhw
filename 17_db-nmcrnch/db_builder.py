4b75451a4912791cdeea592bbccfb4c7b486ac35
raunakchowdhury <rchowdhury5@stuy.edu>
2018-10-05 20:50:08 -0400

    finsihed k17

---
+++ 17_db-nmcrnch/db_builder.py
@@ -0,0 +1,49 @@
+#Raunak Chowdhuey
+#SoftDev1 pd8
+#SQLITE3 BASICS
+#2018-10-04
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+#INSERT YOUR POPULATE CODE IN THIS ZONE
+# courses.csv
+with open('raw/courses.csv') as csvfile: #open csv file and store as DictReader
+    reader = csv.DictReader(csvfile) #{header: element, header2, element2,..}
+    command = 'CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)'
+    c.execute(command)
+    for lines in reader:
+        #print(lines.keys())
+        #print(lines)
+        #print(lines.values())
+        command = 'INSERT INTO courses VALUES(\"{}\",{},{})'.format(lines['code'], int(lines['id']), int(lines['mark'])) #you MUST have quotes for TEXt entries!
+        #print(command)
+        c.execute(command)
+
+with open('raw/occupations.csv') as csvfile: #open csv file and store as DictReader
+    reader = csv.DictReader(csvfile) #{header: element, header2, element2,..}
+    command = 'CREATE TABLE occupations (Job Class TEXT, Percentage INTEGER)'
+    c.execute(command)
+    for lines in reader:
+        cleaned_job = lines['Job Class'].strip('\"') #strip all unnescessary quotes
+        command = 'INSERT INTO occupations VALUES(\"{}\",{})'.format(cleaned_job, float(lines['Percentage']))
+        c.execute(command)
+
+with open('raw/peeps.csv') as csvfile: #open csv file and store as DictReader
+    reader = csv.DictReader(csvfile) #{header: element, header2, element2,..}
+    command = 'CREATE TABLE peeps (name TEXT, age INTEGER, id INTEGER)'
+    c.execute(command)
+    for lines in reader:
+        command = 'INSERT INTO peeps VALUES(\"{}\",{},{})'.format(lines['name'], int(lines['age']), int(lines['id']))
+        c.execute(command)
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -2,3 +2,151 @@
 # Softdev1 pd8
 # K#17: Average
 #2018-10-05
+
+import sqlite3
+
+DB_FILE="discobandit.db"
+
+def lookup_grades(student):
+    '''
+    Lists the grades for one student in all subjects.
+    WARNING: Namespace conflicts will prevent this function from working properly
+    '''
+    return_string = 'Grades of {}:\n'.format(student) # will contain all the relevant info
+    student = '\"{}\"'.format(student) # necessary for comparing names
+
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops
+
+    command = 'SELECT code, mark FROM peeps, courses WHERE peeps.id = courses.id AND name = {}'.format(student) #find courses taken by that student
+    c.execute(command)
+
+    # each entry is organized as (code,mark)
+    for entry in c:
+        return_string += 'Course: {} Grade: {}\n'.format(entry[0], entry[1])
+
+    db.close()
+    return return_string
+
+def compute_average(student):
+    '''
+    Computes the average for one student.
+    WARNING: Namespace conflicts will prevent this function from working properly
+    '''
+    student = '\"{}\"'.format(student) # necessary for comparing names
+    average = 0
+    num_subjects = 0 # keeps track of the number of subjects taken
+
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops
+
+    command = 'SELECT mark FROM peeps, courses WHERE peeps.id = courses.id AND name = {}'.format(student) #find courses taken by that student
+    c.execute(command)
+
+    # each entry is organized as (code,mark)
+    for entry in c:
+        average += entry[0]
+        num_subjects += 1
+
+    db.close()
+    return average / num_subjects
+
+def display_info():
+    '''
+    Extracts the name, average, and id of each user in the sqlite3 db and displays it.
+    '''
+    return_string = '' # will contain all the relevant info
+
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops
+
+    # Grade lookup
+    command = 'SELECT name, courses.mark, peeps.id FROM peeps, courses WHERE peeps.id = courses.id' #Match ids and then report the name of the courses taken
+    c.execute(command)
+
+    name_dict = {} # dict will store values as {'name': [grade, # courses taken, id]}
+    #each row is (name, mark, id)
+    for row in c:
+        if row[0] not in name_dict:
+            name_dict[row[0]] = [row[1], 1, row[2]]
+        else:
+            # increment the # courses taken by 1 and add the mark to the current grade total
+            entry = name_dict[row[0]]
+            entry[0] += row[1]
+            entry[1] += 1
+
+    # turn the num value of the dict into the average
+    for key in name_dict.keys():
+        info_list = name_dict[key] #list that contains [grade, # courses, id]
+        info_list[0] = info_list[0] / info_list[1]
+        return_string += 'Name: {} Average: {} ID: {}\n'.format(key, info_list[0], info_list[2])
+
+    db.close()
+    return return_string
+
+def generate_average_table():
+    '''
+    Extracts the name, average, and id of each user in the sqlite3 db and generates a table that is then stored in the db.
+    '''
+
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops
+
+    # Grade lookup
+    command = 'SELECT name, courses.mark, peeps.id FROM peeps, courses WHERE peeps.id = courses.id' #Match ids and then report the name of the courses taken
+    c.execute(command)
+
+    name_dict = {} # dict will store values as {'name': [grade, # courses taken, id]}
+
+    #each row is (name, mark, id)
+    for row in c:
+        if row[0] not in name_dict:
+            name_dict[row[0]] = [row[1], 1, row[2]]
+        else:
+            # increment the # courses taken by 1 and add the mark to the current grade total
+            entry = name_dict[row[0]]
+            entry[0] += row[1]
+            entry[1] += 1
+
+    # create the table or not depending on whether if its there or not
+    command = 'CREATE TABLE peeps_avg(id INTEGER, average FLOAT) '
+    c.execute(command)
+
+    # turn the num value of the dict into the average
+    for key in name_dict.keys():
+        info_list = name_dict[key] #list that contains [grade, # courses, id]
+        info_list[0] = info_list[0] / info_list[1]
+        #insert the values in as (id, avg)
+        command = 'INSERT INTO peeps_avg VALUES({},{})'.format(info_list[2], info_list[0])
+        c.execute(command)
+
+    db.commit()
+    db.close()
+
+def add_course(code, new_id, mark):
+    '''
+    Adds a new row to the database with the parameters code, id, and mark.
+    code --> string
+    id --> integer
+    mark --> integer
+    '''
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops
+
+    code = '\"{}\"'.format(code) # necessary for comparing names
+
+    # find and update the peeps_avg table with the new value
+    command = 'INSERT INTO courses VALUES({},{},{})'.format(code, new_id, mark)
+    c.execute(command)
+
+    db.commit()
+    db.close()
+
+if __name__ == '__main__':
+    generate_average_table()
+    print(lookup_grades('kruder'))
+    print('Average of kruder: {}'.format(compute_average('kruder')))
+    add_course('apcs', 1, 95)
+    print('Average of kruder after adding apcs: {}'.format(compute_average('kruder')))
+    print(lookup_grades('kruder'))
+    print('Printing info for all students:\n{}'.format(display_info()))
